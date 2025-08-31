11c752ce5a53cdc0da3079057cd9f27f18211db5
Armaan Nagra <armaandeepnagra@icloud.com>
2025-08-31 10:46:52 +0100

    implement subtraction of 2 matrices

--- matrixLib.cpp
+++ matrixLib.cpp
@@ -1,6 +1,7 @@
 #include <cstddef>
 #include <vector>
 #include <iostream>
+#include <stdexcept>
 
 class Matrix{
 public:
@@ -32,8 +33,21 @@ public:
         return result;
     }
 
-    
+    Matrix operator-(const Matrix& other) const {
+        if(rows_ != other.rows() || cols_ != other.cols()){
+            throw std::invalid_argument("Matrix dimensions need to match for subtracting");
+        }
 
+        Matrix result(rows_, cols_);
+
+        for(size_t i = 0; i < rows_; ++i){
+            for (size_t j = 0; j < cols_; ++i){
+                result(i,j) = (*this)(i,j) - other(i,j);
+            }
+        }
+    }
+
+    
 private: 
     size_t rows_, cols_;
     std::vector<double> data_;
@@ -65,7 +79,7 @@ int main(){
             std::cout << A(i,j) << (j + 1 < 3 ? ' ' : '\n');
         }
     }
-    
+
     for (size_t i = 0; i < 2; ++i){
         for (size_t j = 0; j < 3; j++){
             std::cout << C(i,j) << (j + 1 < 3 ? ' ' : '\n');
